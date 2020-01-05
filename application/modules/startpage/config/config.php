a3018c7a855d6bde0addd7cf1054d407da68df3a
Slipi85 <52356917+Slipi85@users.noreply.github.com>
2020-01-05 01:00:54 +0100

    Update config.php

--- application/modules/startpage/config/config.php
+++ application/modules/startpage/config/config.php
@@ -35,7 +35,8 @@ class Config extends \Ilch\Config\Install
 
     public function uninstall()
     {
-      $this->db()->queryMulti('DROP TABLE `[prefix]_startpage`;');
+      $this->db()->queryMulti('DROP TABLE `[prefix]_startpage`;
+                               DROP TABLE `[prefix]_startpage_col`;');
     }
 
     public function getInstallSql()
@@ -61,7 +62,27 @@ class Config extends \Ilch\Config\Install
               `color_grid` VARCHAR(255) NOT NULL,
               `function` MEDIUMTEXT NOT NULL,
       				PRIMARY KEY (`id`)
-      				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1;';
+      		) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1;
+
+          CREATE TABLE IF NOT EXISTS `[prefix]_startpage_col` (
+              `id` Int(11) NOT NULL AUTO_INCREMENT,
+              `ilch-col` VARCHAR(55) NOT NULL,
+              PRIMARY KEY (`id`)
+      				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1;
+
+          INSERT INTO `[prefix]_startpage_col` (`id`, `ilch-col`) VALUES
+              (1, "ilch-col-1"),
+              (2, "ilch-col-2"),
+              (3, "ilch-col-3"),
+              (4, "ilch-col-4"),
+              (5, "ilch-col-5"),
+              (6, "ilch-col-6"),
+              (7, "ilch-col-7"),
+              (8, "ilch-col-8"),
+              (9, "ilch-col-9"),
+              (10, "ilch-col-10"),
+              (11, "ilch-col-11"),
+              (12, "ilch-col-12");';
     }
     public function getUpdate($installedVersion)
     {
